b9daccf6c23bf9ea1f566a2fe8b294e1b8db2de3
Riju Bhattacharyya <74527389+rijub98@users.noreply.github.com>
2024-06-18 10:55:33 +0530

    Delete JAVASCRIPT directory

--- JAVASCRIPT/input.js
+++
@@ -1,7 +0,0 @@
-const textarea = document.querySelector("textarea");
-
-textarea.addEventListener("keyup", e=>{
-    textarea.style.height = "59px";
-    let scHeight = e.target.scrollHeight;
-    textarea.style.height = `${scHeight}px`;
-});
